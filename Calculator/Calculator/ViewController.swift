31941bae4a55d0e5f2f777acbe5087272d00db53
seanecho <seanecho@outlook.com>
2015-05-04 16:02:23 +0800

    finish the optimization
    1.用到swift中的变量推断
    2.swift中引用函数时可以不用定义变量，会自己给出栈中的元素：$0,$1,$2.....
    3.引用函数时，函数的最后一个参数可以写在圆括号后面，如果函数只有一个参数的话，括号里就是空的，当这样的情况时，圆括号也可以去掉

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -30,26 +30,28 @@ class ViewController: UIViewController {
         }
         
         switch operation{
-        case "×": performOperation(multiply)
-//        case "÷":performOperation(divide)
-//        case "+":performOperation(add)
-//        case "−":performOperation(subtract)
+        case "×":performOperation(){$0 * $1}
+        case "÷":performOperation(){$1 / $0}
+        case "+":performOperation(){$0 + $1}
+        case "−":performOperation(){$1 - $0}
+        case "√":performOperation1(){sqrt($0)}
         default:break
         
         }
     }
         
-        func performOperation(operation: (Double,Double) -> Double){
-            if operandStack.count >= 2 {
+    func performOperation(operation: (Double,Double) -> Double){
+        if operandStack.count >= 2 {
                 displayValue = operation(operandStack.removeLast(),operandStack.removeLast())
                 enter()
             }
         }
-        
-        func multiply(op1: Double , op2: Double) -> Double{
-            return op1 * op2
+    func performOperation1(operation: Double -> Double){
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
         }
-    
+    }
 
     var operandStack = Array<Double>()
     @IBAction func enter(){
